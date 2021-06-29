05ac85ed704d8bb9f15ebe67e921090895a35fa1
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2021-06-29 13:22:02 +0200

    Programmatic Navigation to edit page

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Params } from "@angular/router";
+import { ActivatedRoute, Params, Router } from "@angular/router";
 
 import { Recipe } from "../recipe.model";
 import { RecipeService } from "../recipe.service";
@@ -15,7 +15,8 @@ export class RecipeDetailComponent implements OnInit {
 
   constructor(
     private recipeService: RecipeService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -23,6 +24,16 @@ export class RecipeDetailComponent implements OnInit {
       this.id = +params["id"];
       this.recipe = this.recipeService.getRecipe(this.id);
     });
+  }
+
+  to;
+  onEditRecipe(): void {
+    this.router.navigate(["edit"], {
+      relativeTo: this.activatedRoute,
+    });
+    // this.router.navigate(["../", this.id, "edit"], {
+    //   relativeTo: this.activatedRoute,
+    // });
   }
 
   onAddToShoppingList() {
